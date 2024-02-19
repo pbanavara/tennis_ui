392ebf5d60ad80b5811402182f91a6201c57571d
Pradeep Banavara <pradeepbs@gmail.com>
2024-02-19 18:31:59 +0530

    Fully working front end app, not yet deployed to prod: TODO : add server logic for handling email and remove hardcoded video file name for player's video

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react'
+
+export default function Header() {
+    const { data: session } = useSession()
+    async function signOutFromHere(e) {
+        e.preventDefault()
+        await signOut()
+    }
+    return (
+        <header className="flex justify-between text-black items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+            <Link href="/" className="flex space-x-2">
+                <h2 className="sm:text-5xl text-3xl font-bold ml-2 tracking-tight">
+                    callindra.com
+                </h2>
+            </Link>
+            <div className="flex space-x-6">
+                <a className="border-r border-gray-300 pr-4 space-x-2 hover:text-blue-400 transition hidden sm:flex" href="/">
+                    <p className="font-medium text-base">Home</p>
+                </a>
+                {session ? (
+                    <a className="border-gray-300 pr-4 space-x-2 hover:text-blue-400 transition hidden sm:flex" onClick= {signOutFromHere }> {session.user.name}</a>
+                ): ( 
+                <a className="border-gray-300 pr-4 space-x-2 hover:text-blue-400 transition hidden sm:flex" href="/ace">
+                    <p class="font-medium text-base">Ace</p>
+                        </a>
+                )
+            }
+            </div>
+        </header>
+    );
+}
---
+++ pages/ace.tsx
@@ -0,0 +1,109 @@
+"use client"
+import React from 'react';
+import axios from "axios";
+import { NextPage } from 'next'
+import { useState } from 'react';
+import { FormEvent } from 'react';
+import Header from "../components/Header";
+import { useSession, signIn } from 'next-auth/react';
+import  Image from 'next/image';
+
+export default function Ace() {
+    const [fileObj, setFileObj] = useState<File>();
+    const [userEmail, setUserEmail] = useState<string>();
+    const { data: session } = useSession();
+
+    let handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let files = e.currentTarget.files
+        if (files) {
+            setFileObj(files[0])
+            setUserEmail(session.user.email)
+        }
+    }
+    let uploadFile = () => {
+        var formData = new FormData();
+        formData.append('email', userEmail)
+        formData.append('file', fileObj)
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            },
+            withCredentials: false
+        } 
+        try {
+            axios.post("http://localhost:8000/video", formData, config).then((response) => {
+                console.log("Response is :: ", response.data);
+            },
+            (error) => {
+                console.log("An error occured in upload", error)
+            }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+    if (!session) {
+    return (
+        <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+            <Header />
+            <main className="flex flex-1 w-full flex-col items-center text-center px-2 sm:mt-28 mt-20">
+                <div className="h-[250px] flex flex-col items-center space-y-6 max-w-[670px] -mt-8">
+                    <div className="max-w-xl text-gray-600">Sign in below with Google to create a free account and unalyze your videos today.
+                    </div>
+                    <button
+                        onClick={() => signIn("google")}
+                        className="bg-gray-200 text-black font-semibold py-3 px-6 rounded-2xl flex items-center space-x-2">
+                        <Image
+                            src="/google.png"
+                            width={20}
+                            height={20}
+                            alt="google's logo"
+                        />
+                        <span>Sign in with Google</span>
+                    </button>
+                </div>
+            </main>
+        </div>
+    )
+    } else {
+        console.log("User is authenticated");
+        return (
+            <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+                <Header />
+                <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-2 sm:mt-28 mt-20">
+                    <h2 className = "text-black" > Upload your videos here
+                    </h2>
+                    <div>
+
+                    <input className = "text-black" type = "file" name = "file" accept = "video/*" required onChange = { handleFileUpload } />
+                    <button className="bg-white rounded-xl text-black font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-gray-100 border" type="submit" onClick={uploadFile}>Upload
+                        </button>
+                    
+                    </div>
+                    <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
+                        <div className="flex flex-col space-y-10 mt-4 mb-16">
+                            <div className="flex sm:space-x-2 sm:flex-row flex-col">
+                                <div>
+                                    <video width="800" height="600" controls autoPlay={true} loop >
+                                        <source src="http://d3dgc1hn06lo53.cloudfront.net/output_1.m4v" type="video/mp4" />
+                                    </video>
+                                </div>
+                                <div>
+                                    <video width="800" height="600" controls autoPlay={true} loop >
+                                        <source src="http://d3dgc1hn06lo53.cloudfront.net/fed_out.m4v" type="video/mp4" />
+                                        Your browser does not support the video tag.
+                                    </video>
+                                </div>
+                                <div className="sm:mt-0 mt-8">
+                                    <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                </main>
+            </div>
+        )
+    }
+
+}
