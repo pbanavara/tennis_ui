dca29367537df561be19a47f75b153340f238010
Pradeep Banavara <pradeepbs@gmail.com>
2024-02-26 10:54:44 +0530

    Added GA analytics and state management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react"
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <SessionProvider session={pageProps.session}>
                 <Component {...pageProps} />
-                <Analytics />
+                <GoogleAnalytics gaId="G-F1JBCQ0MHY" />
             </SessionProvider>
             
         </>
